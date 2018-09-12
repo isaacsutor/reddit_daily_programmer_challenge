ee205b3b69122c362e605a949444c64264a30f2a
isaac_sutor <isaac.sutor.15@cnu.edu>
2018-09-12 10:42:18 -0400

    Reddit Daily Challenge #366 Word Funnel: Complete

--- word_funnel.py
+++ word_funnel.py
@@ -1,1 +1,36 @@
+# Word Funnel Challenge:
+# Given 2 strings of letters, determine whether the second can
+# be made from the first by removing one letter. The remaining
+# letters must stay in the same order.
+# Author: isaacsutor
+# Version: 2018912
+
+# Solution Ex.
+# funnel("leave", "eave") => true
+# funnel("reset", "rest") => true
+# funnel("dragoon", "dragon") => true
+# funnel("eave", "leave") => false
+# funnel("sleet", "lets") => false
+# funnel("skiff", "ski") => false
+
+
+def funnel(word_one, word_two):
+    found_match = False
+    i = 0
+    for letter in word_one:
+        if i-1 < len(word_one):
+            compare_with = word_one[:i] + word_one[i+1:]
+            i += 1
+            if compare_with == word_two:
+                found_match = True
+    print(found_match)
+    return found_match
+
+
+funnel("leave", "eave")
+funnel("reset", "rest")
+funnel("dragoon", "dragon")
+funnel("eave", "leave")
+funnel("sleet", "lets")
+funnel("skiff", "ski")
 
