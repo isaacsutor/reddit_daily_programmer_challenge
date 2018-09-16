094b597cca3ed5cef520d83118d2fd517bdfc007
isaac_sutor <isaac.sutor.15@cnu.edu>
2018-09-16 13:59:23 -0400

    Reddit Daily Challenge #348 The Bunny Problem: Project challenge added

---
+++ rabbit_problem.py
@@ -0,0 +1,18 @@
+# The Rabbit Problem:
+# Description
+# Rabbits are known for their fast breeding, but how soon will they dominate the earth?
+# Starting with a small population of male and female rabbits we have to figure out how long it will take for them to outnumber humans 2:1.
+# Every month a fertile female will have 14 offspring (5 males and 9 females).
+# A female rabbit is fertile when it has reached the age of 4 months, they never stop being fertile.
+# Rabbits die at the age of 8 years (96 months).
+# Input description
+# You will be given a list of numbers as following:
+# Male_rabbits Female_rabbits Rabbits_needed_alive
+# The initial rabbits will always be 2 months old and fertile females will always produce 14 offspring (5 male, 9 female)
+# Every month that passes things should be done in this order:
+# Fertile female reproduce	(so 7 year & 11 months old will reproduce)
+# rabbits age (except newborn) (and rabbits reaching 8 years will die, the 7 year & 11 months old will die)
+# Author: isaacsutor
+# Version: 2018916
+
+
