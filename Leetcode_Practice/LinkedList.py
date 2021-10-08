6f31d6325a8a56b76a7cb7cc526b533dab9e5c6b
isaacsutor <isaac.sutor@gmail.com>
2021-10-08 09:25:08 -0400

    linked list problem 876 completed

--- Leetcode_Practice/LinkedList.py
+++ Leetcode_Practice/LinkedList.py
@@ -3,6 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+class MiddleOfTheLinkedList876:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # 3:25 Time
+        # 32 ms | 69%
+        # 14.2MB  | 43%
+        front_runner = head
+        back_runner = head
+        while(front_runner.next != None and front_runner.next.next != None):
+            front_runner = front_runner.next.next
+            back_runner = back_runner.next
+            
+        if(front_runner.next != None):
+            return back_runner.next
+        return back_runner
+
+
+
+
+
+
+
 class ConvertBinaryNumberInLinkedListToInteger1290:
     def getDecimalValue(self, head: ListNode) -> int:
         # Brute Force 11:45 Time
